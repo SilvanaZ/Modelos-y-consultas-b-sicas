26131996c70b7b5c1ea1e4dbeff0fe57fbbddaea
silvana zalazar <silvana.blaq@gmail.com>
2022-01-18 17:09:09 -0300

    update 2

--- materiales/src/controllers/moviesController.js
+++ materiales/src/controllers/moviesController.js
@@ -1,8 +1,25 @@
 const db = require('../database/models');
 
-modules.exports ={
-list: (req, res) =>{
-    db.genre.findAll()
-    .then((genres) =>res.send(genres) );
-}
+modules.exports = {
+    list: (req, res) => {
+        db.genre.findAll()
+            .then((genres) => res.send(genres));
+    },
+    detail: (req, res) => { 
+        db.Movie.findByPk(req.params.id)
+        .then((movie) => { 
+            res.render('moviesDetail', { movie 
+            }) 
+        }) 
+    }, 
+    new: (req, res) => { 
+        db.Movie.findAll({ 
+            order: [['release_date', 'DESC']] 
+        })
+        .then((movies) => { 
+            res.render('newestMovies', { 
+                movies 
+            }) 
+        }) 
+    }
 }
